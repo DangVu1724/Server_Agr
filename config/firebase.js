7ba003c09c59995ae0400fc75b92dd8802e07144
DangVu1724 <dangvu0107@gmail.com>
2025-10-19 20:55:29 +0700

    fix

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,14 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../agrimarket-60402-firebase-adminsdk-fbsvc-8a00525017.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+  });
+}
 
 const db = admin.firestore();
 const FieldValue = admin.firestore.FieldValue;  
