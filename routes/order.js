cb78a811ad901e714dc4a1908cc0ba5b0a2a75a8
DangVu1724 <dangvu0107@gmail.com>
2025-11-13 21:08:04 +0700

    fix method

--- routes/order.js
+++ routes/order.js
@@ -7,7 +7,7 @@ const { verifyToken } = require('../middleware/auth');
 
 router.post('/', verifyToken, createOrder);
 router.put('/:orderId/status', updateOrderStatus);
-router.put('/:orderId/cancel', verifyToken, require('../controllers/orderController').cancelOrderByBuyer);
+router.post('/:orderId/cancel', verifyToken, require('../controllers/orderController').cancelOrderByBuyer);
 
 
 module.exports = router;
